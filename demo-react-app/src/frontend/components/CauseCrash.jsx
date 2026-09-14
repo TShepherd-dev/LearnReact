7d8bc7c82589628548dbdc652807a5dbb68ac7eb
Tim Shepherd <tshepherd12.dev@gmail.com>
2026-09-14 13:58:13 +1000

    Add alert before deliberate crash in CauseCrash component

--- demo-react-app/src/frontend/components/CauseCrash.jsx
+++ demo-react-app/src/frontend/components/CauseCrash.jsx
@@ -1,5 +1,6 @@
 export default function MembersAddUser({ userForm, setUserForm }) {
     const causeCrash = () => {
+        alert("About to cause a crash");
         throw new Error("This is a deliberate crash");
     };
     return (
